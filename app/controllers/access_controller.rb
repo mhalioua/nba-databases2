458598049ac075b6ae11a4f509342afc48b09754
higherstar <higherstar@outlook.com>
2017-09-18 07:37:07 -0500

    again

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -17,7 +17,7 @@
   		session[:user_id] = authorized_user.id
   		session[:username] = authorized_user.username
   		flash[:notice] = "You are now logged in."
-  		redirect_to(:controller => 'index', :action => 'home')
+  		redirect_to(:controller => 'welcome', :action => 'index')
   	else
   		flash[:notice] = "Invalid username/password combination."
   		redirect_to(:action => 'login')
