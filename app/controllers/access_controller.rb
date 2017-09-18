611ce983e65b99813efcd3b765f027c969b7059f
higherstar <higherstar@outlook.com>
2017-09-18 07:42:01 -0500

    again

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -8,14 +8,14 @@
 
   def attempt_login
   	if params[:username].present? && params[:password].present?
-  		found_user = User.where(:username => params[:username]).first
+  		found_user = User.where(:user => params[:username]).first
   		if found_user
   			authorized_user = found_user.authenticate(params[:password])
   		end
   	end
   	if authorized_user
   		session[:user_id] = authorized_user.id
-  		session[:username] = authorized_user.username
+  		session[:username] = authorized_user.user
   		flash[:notice] = "You are now logged in."
   		redirect_to(:controller => 'welcome', :action => 'index')
   	else
