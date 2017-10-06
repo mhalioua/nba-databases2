a3fab74873cdcc572b59eefea7aec263b4fbec8b
higherstar <higherstar@outlook.com>
2017-10-06 08:57:07 +0000

    again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,7 @@
 //= require jquery
 
 //= require bootstrap-datepicker
+
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.es
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.fr
