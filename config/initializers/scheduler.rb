6a31d61d4ac523d8c37d22fded70242a33370668
higherstar <higherstar@outlook.com>
2017-10-11 07:59:01 +0000

    again

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,9 +1,3 @@
 require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler::singleton
-
-
-scheduler.every '5s' do
-  # do stuff
-  User.create(name: "a", password: "a", password_confirmation: "a")
-end
