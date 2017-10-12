d10baeca7449df026b9fadf77e4a3e2947ac2d2b
higherstar <higherstar@outlook.com>
2017-10-12 07:00:25 +0000

    again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,7 @@ class WelcomeController < ApplicationController
 	  	  	params[:id] = Time.now.strftime("%Y-%m-%d") + " - " + Time.now.strftime("%Y-%m-%d")
 	  	end
 	  	@game_index = params[:id]
-	  	@game_start_index = @game_index[0..10]
+	  	@game_start_index = @game_index[0..9]
 	  	@game_end_index = @game_index[13..23]
 	  	@games = Game.where("game_date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
 	  				.order("game_state")
