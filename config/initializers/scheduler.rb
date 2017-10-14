9ce47652dfffa176a533d1c7a89d9da116f1ce0b
higherstar <higherstar@outlook.com>
2017-10-14 10:08:06 +0000

    try

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,6 +3,5 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler::singleton
 
 scheduler.every '10s' do
-	Rake::Task["setup:tensecond"].invoke
-	Rake::Task["setup:tensecond"].reenable
+	rake "setup:tensecond"
 end
