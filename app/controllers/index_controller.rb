64c3c2ac8f2fc88240682ae1e20cd6d1430fd805
higherstar <higherstar@outlook.com>
2017-10-25 23:41:20 +0000

    test

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,15 @@
+class IndexController < ApplicationController
+	def home
+		@yesterday = Time.now - 1.days
+    	@today = Time.now
+    	@tomorrow = Time.now + 1.days
+  	end
+
+	def game
+	  	unless params[:id]
+	  	  	params[:id] = Time.now.strftime("%Y%m%d")
+	  	end
+	  	
+		@games = params[:id]
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	
-  root 'welcome#index'
+  root 'index#index'
 
   match ':controller(/:action(/:id))', :via => [:get, :post]
 end
