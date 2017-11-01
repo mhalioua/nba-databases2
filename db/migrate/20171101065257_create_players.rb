433614aefc66d0cd4b12e6e89093211b768f0d03
higherstar <higherstar@outlook.com>
2017-11-01 08:36:42 +0000

    again

--- db/migrate/20171101065257_create_players.rb
+++ db/migrate/20171101065257_create_players.rb
@@ -1,6 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration[5.1]
   def change
     create_table :players do |t|
+        t.integer :game_id
         t.integer :team_abbr
 		    t.string :player_name
       	t.string :position
