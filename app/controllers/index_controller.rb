4a3f3d6a4ee41c294ef6f723c7a5d34e7e9f4bec
higherstar <higherstar@outlook.com>
2017-11-01 17:04:25 +0000

    again

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,8 +10,9 @@ class IndexController < ApplicationController
 	  	unless params[:id]
 	  	  	params[:id] = Time.now.strftime("%Y%m%d")
 	  	end
-	  	
 		@game_index = params[:id]
+
+  		@head = Date.strptime(@game_index, '%Y%m%d').strftime("%B %e")
 		@games = Nba.where("game_date between ? and ?", Date.strptime(@game_index, '%Y%m%d').beginning_of_day, Date.strptime(@game_index, '%Y%m%d').end_of_day)
 	  				.order("game_date")
 	end
