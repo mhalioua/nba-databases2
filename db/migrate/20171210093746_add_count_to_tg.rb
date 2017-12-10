3ce1b868333902febc9895602d8f8b73c8eb6a64
higherstar <higherstar@outlook.com>
2017-12-10 09:50:10 +0000

    Fix

--- db/migrate/20171210093746_add_count_to_tg.rb
+++ db/migrate/20171210093746_add_count_to_tg.rb
@@ -1,7 +1,7 @@
 class AddCountToTg < ActiveRecord::Migration[5.1]
   def change
   	add_column :tgs, :count, :integer
-  	change_column  :player, :ortg, :float
-  	change_column  :player, :drtg, :float
+  	change_column  :players, :ortg, :float
+  	change_column  :players, :drtg, :float
   end
 end
