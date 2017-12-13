706140f9d5d5cdb0c8a4e4d60cc1df8dbb205a52
higherstar <higherstar@outlook.com>
2017-12-13 08:59:01 +0000

    fix

--- db/migrate/20171212232841_add_link_to_tg.rb
+++ db/migrate/20171212232841_add_link_to_tg.rb
@@ -1,7 +1,7 @@
 class AddLinkToTg < ActiveRecord::Migration[5.1]
   def change
-  	change_column :tgs, :player_link, :string
-  	change_column :tgs, :player_fullname, :string
+  	add_column :tgs, :player_link, :string
+  	add_column :tgs, :player_fullname, :string
 
   	add_column :players, :player_link, :string
   	add_column :players, :player_fullname, :string
