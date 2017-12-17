05e34d9a36c1ff1bb15b9a1e864beb08b86d1eed
higherstar <higherstar@outlook.com>
2017-12-17 20:27:51 +0000

    fix

--- db/migrate/20171217201605_add_info_to_team.rb
+++ db/migrate/20171217201605_add_info_to_team.rb
@@ -1,8 +1,11 @@
 class AddInfoToTeam < ActiveRecord::Migration[5.1]
   def change
   	add_column :teams, :timezone, :integer
-	add_column :teams, :order_one, :integer
-	add_column :teams, :order_two, :integer
-	add_column :teams, :order_thr, :integer
+	add_column :teams, :order_one_sev, :integer
+	add_column :teams, :order_two_sev, :integer
+	add_column :teams, :order_thr_sev, :integer
+	add_column :teams, :order_one_six, :integer
+	add_column :teams, :order_two_six, :integer
+	add_column :teams, :order_thr_six, :integer
   end
 end
