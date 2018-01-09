65dafca36461ada381e522953685fb85c00cd9a9
Apollo <super.hero2017@outlook.com>
2018-01-09 09:57:09 +0000

    fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -945,72 +945,49 @@ class IndexController < ApplicationController
 
 
 	    @filters = [
-			[true, false, false, true, true, true],
-			[false, true, true, false, true, true],
-			[true, true, false, false, true, true],
-			[false, false, true, true, true, true],
-
-			[true, false, false, true, true, false],
-			[false, true, true, false, true, false],
-			[true, true, false, false, true, false],
-			[false, false, true, true, true, false],
-
-			[true, false, false, true, false, true],
-			[false, true, true, false, false, true],
-			[true, true, false, false, false, true],
-			[false, false, true, true, false, true],
-
-			[false, false, true, false, true, true],
-			[false, true, false, false, true, true],
-
-			[false, true, true, true, true, true],
-			[true, false, true, true, true, true],
-			[true, true, false, true, true, true],
-			[true, true, true, false, true, true],
-
-			[false, true, true, true, true, false],
-			[true, false, true, true, true, false],
-			[true, true, false, true, true, false],
-			[true, true, true, false, true, false],
-
-			[false, true, true, true, false, true],
-			[true, false, true, true, false, true],
-			[true, true, false, true, false, true],
-			[true, true, true, false, false, true],
-
-			[false, true, true, false, false, false],
-
-			[true, true, true, true, true, true],
-
-			[true, true, true, true, false, false]
+	    	[false, true, true, true, true, true, true, true],
+	    	[true, false, true, true, true, true, true, true],
+	    	[true, true, false, true, true, true, true, true],
+	    	[true, true, true, false, true, true, true, true],
+	    	[true, true, true, true, false, true, true, true],
+	    	[true, true, true, true, true, false, true, true],
+	    	[true, true, true, true, true, true, false, true],
+	    	[true, true, true, true, true, true, true, false],
+	    	[true, true, false, true, true, false, true, true],
+	    	[true, true, true, false, false, true, true, true],
+	    	[false, false, true, true, true, true, true, true],
+	    	[true, true, true, true, true, true, false, false],
+	    	[false, true, true, true, true, true, true, false],
+	    	[true, false, true, true, true, true, false, true],
+	    	[false, false, true, true, true, true, false, false]
 		]
-		@break = [3, 7, 11, 13, 17, 21, 25, 26, 27, 28]
+		@break = [7, 13, 14]
 		@filterResult = []
 		@filters.each do |filter|
 			search_string = []
-			if filter[0]
-				search_string.push("roadlast = '#{@game.away_last_game}'")
-				filter[0] = @game.away_last_game
-			end
-			if filter[1]
-				search_string.push("roadnext = '#{@game.away_next_game}'")
-				filter[1] = @game.away_next_game
-			end
 			if filter[2]
-				search_string.push("homenext = '#{@game.home_next_game}'")
-				filter[2] = @game.home_next_game
+				search_string.push("roadlast = '#{@game.away_last_game}'")
+				filter[2] = @game.away_last_game
 			end
 			if filter[3]
-				search_string.push("homelast = '#{@game.home_last_game}'")
-				filter[3] = @game.home_last_game
+				search_string.push("roadnext = '#{@game.away_next_game}'")
+				filter[3] = @game.away_next_game
 			end
 			if filter[4]
-				search_string.push("homenextfly = '#{@game.home_next_fly}'")
-				filter[4] = @game.home_next_fly[0]
+				search_string.push("homenext = '#{@game.home_next_game}'")
+				filter[4] = @game.home_next_game
 			end
 			if filter[5]
+				search_string.push("homelast = '#{@game.home_last_game}'")
+				filter[5] = @game.home_last_game
+			end
+			if filter[6]
+				search_string.push("homenextfly = '#{@game.home_next_fly}'")
+				filter[6] = @game.home_next_fly[0]
+			end
+			if filter[7]
 				search_string.push("homelastfly = '#{@game.home_last_fly}'")
-				filter[5] = @game.home_last_fly[0]
+				filter[7] = @game.home_last_fly[0]
 			end
 			search_string = search_string.join(" AND ")
 			filter_element = Fullseason.where(search_string)
