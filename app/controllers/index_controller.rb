999f69cd53db559878ba5e02a7184accc598147c
Apollo <super.hero2017@outlook.com>
2018-01-29 06:44:53 +0800

    Fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -21,62 +21,65 @@ class IndexController < ApplicationController
 	end
 
 	def referee
-		@referee_filter_third = []
-		(0..6).each do |one_element|
-			@referee_filter_second = []
-			(0..6).each do |two_element|
-				@referee_filter_first = []
-				(0..6).each do |three_element|
-					search_array_last = []
-					search_array_next = []
-					if one_element > 5
-						search_array_last.push("referee_one_last > 5")
-						search_array_next.push("referee_one_next > 5")
-					else
-						search_array_last.push("referee_one_last = #{one_element}")
-						search_array_next.push("referee_one_next = #{one_element}")
+		if false
+			@referee_filter_third = []
+			(0..6).each do |one_element|
+				@referee_filter_second = []
+				(0..6).each do |two_element|
+					@referee_filter_first = []
+					(0..6).each do |three_element|
+						search_array_last = []
+						search_array_next = []
+						if one_element > 5
+							search_array_last.push("referee_one_last > 5")
+							search_array_next.push("referee_one_next > 5")
+						else
+							search_array_last.push("referee_one_last = #{one_element}")
+							search_array_next.push("referee_one_next = #{one_element}")
+						end
+						if two_element > 5
+							search_array_last.push("referee_two_last > 5")
+							search_array_next.push("referee_two_next > 5")
+						else
+							search_array_last.push("referee_two_last = #{two_element}")
+							search_array_next.push("referee_two_next = #{two_element}")
+						end
+						if three_element > 5
+							search_array_last.push("referee_three_last > 5")
+							search_array_next.push("referee_three_next > 5")
+						else
+							search_array_last.push("referee_three_last = #{three_element}")
+							search_array_next.push("referee_three_next = #{three_element}")
+						end
+						search_array_last = search_array_last.join(" AND ")
+						search_array_next = search_array_next.join(" AND ")
+						referee_filter_result_last = Referee.where(search_array_last)
+						referee_filter_result_next = Referee.where(search_array_next)
+						@referee_filter_first.push([
+							referee_filter_result_last.count(:tp_1h).to_i,
+							referee_filter_result_last.sum(:tp_1h).to_f.round(2),
+							referee_filter_result_last.sum(:tp_2h).to_f.round(2),
+							referee_filter_result_next.count(:tp_1h).to_i,
+							referee_filter_result_next.sum(:tp_1h).to_f.round(2),
+							referee_filter_result_next.sum(:tp_2h).to_f.round(2)
+						])
 					end
-					if two_element > 5
-						search_array_last.push("referee_two_last > 5")
-						search_array_next.push("referee_two_next > 5")
-					else
-						search_array_last.push("referee_two_last = #{two_element}")
-						search_array_next.push("referee_two_next = #{two_element}")
-					end
-					if three_element > 5
-						search_array_last.push("referee_three_last > 5")
-						search_array_next.push("referee_three_next > 5")
-					else
-						search_array_last.push("referee_three_last = #{three_element}")
-						search_array_next.push("referee_three_next = #{three_element}")
-					end
-					search_array_next = search_array_next.join(" AND ")
-					referee_filter_result_next = Referee.where(search_array_next)
-					@referee_filter_first.push([
-						1,
-						0,
-						0,
-						referee_filter_result_next.count(:tp_1h).to_i,
-						referee_filter_result_next.sum(:tp_1h).to_f.round(2),
-						referee_filter_result_next.sum(:tp_2h).to_f.round(2)
-					])
+					@referee_filter_second.push(@referee_filter_first)
 				end
-				@referee_filter_second.push(@referee_filter_first)
+				@referee_filter_third.push(@referee_filter_second)
 			end
-			@referee_filter_third.push(@referee_filter_second)
-		end
-		@result = []
-		(0..6).each do |one_element|
-			(one_element..6).each do |two_element|
-				start_element = two_element
-				start_element = 0 if one_element == 0
-				(start_element..6).each do |three_element|
-					total_last_count = 0
-					total_last_first = 0
-					total_last_second = 0
-					total_next_count = 0
-					total_next_first = 0
-					total_next_second = 0
+			@result = []
+			(0..6).each do |one_element|
+				(one_element..6).each do |two_element|
+					start_element = two_element
+					start_element = 0 if one_element == 0
+					(start_element..6).each do |three_element|
+						total_last_count = 0
+						total_last_first = 0
+						total_last_second = 0
+						total_next_count = 0
+						total_next_first = 0
+						total_next_second = 0
 						total_last_count = total_last_count + @referee_filter_third[one_element][two_element][three_element][0]
 						total_last_count = total_last_count + @referee_filter_third[one_element][three_element][two_element][0]
 						total_last_count = total_last_count + @referee_filter_third[two_element][one_element][three_element][0]
@@ -138,18 +141,19 @@ class IndexController < ApplicationController
 						total_last_second = total_last_second / total_last_count
 						total_next_first = total_next_first / total_next_count
 						total_next_second = total_next_second / total_next_count
-					
-					@result.push([
-						one_element,
-						two_element,
-						three_element,
-						total_last_count,
-						total_last_first.round(2),
-						total_last_second.round(2),
-						total_next_count,
-						total_next_first.round(2),
-						total_next_second.round(2)
-					])
+						
+						@result.push([
+							one_element,
+							two_element,
+							three_element,
+							total_last_count,
+							total_last_first.round(2),
+							total_last_second.round(2),
+							total_next_count,
+							total_next_first.round(2),
+							total_next_second.round(2)
+						])
+					end
 				end
 			end
 		end
