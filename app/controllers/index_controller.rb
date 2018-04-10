7031ed4fc75fdaac2e4ba406381cb16888b2a841
Apollo <super.hero2017@outlook.com>
2018-04-10 02:54:05 -0400

    Fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1144,61 +1144,77 @@ class IndexController < ApplicationController
 		@filters.each_with_index do |filter, index|
 			search_string = []
 			search_second_string = []
-			if filter[0]
-				search_string.push("awaylastfly = '#{@game.away_last_fly}'")
-				search_second_string.push("awaylastfly = '#{@game.away_last_fly}'")
-				filter[0] = @game.away_last_fly[0]
-			else
-				search_string.push("awaylastfly <> '#{@game.away_last_fly}'")
+			if @game.away_last_fly != nil
+				if filter[0]
+					search_string.push("awaylastfly = '#{@game.away_last_fly}'")
+					search_second_string.push("awaylastfly = '#{@game.away_last_fly}'")
+					filter[0] = @game.away_last_fly[0]
+				else
+					search_string.push("awaylastfly <> '#{@game.away_last_fly}'")
+				end
 			end
-			if filter[1]
-				search_string.push("awaynextfly = '#{@game.away_next_fly}'")
-				search_second_string.push("awaynextfly = '#{@game.away_next_fly}'")
-				filter[1] = @game.away_next_fly[0]
-			else
-				search_string.push("awaynextfly <> '#{@game.away_next_fly}'")
+			if @game.away_next_fly != nil
+				if filter[1]
+					search_string.push("awaynextfly = '#{@game.away_next_fly}'")
+					search_second_string.push("awaynextfly = '#{@game.away_next_fly}'")
+					filter[1] = @game.away_next_fly[0]
+				else
+					search_string.push("awaynextfly <> '#{@game.away_next_fly}'")
+				end
 			end
-			if filter[2]
-				search_string.push("roadlast = '#{@game.away_last_game}'")
-				search_second_string.push("roadlast = '#{@game.away_last_game}'")
-				filter[2] = @game.away_last_game
-			else
-				search_string.push("roadlast <> '#{@game.away_last_game}'")
+			if @game.away_last_game
+				if filter[2]
+					search_string.push("roadlast = '#{@game.away_last_game}'")
+					search_second_string.push("roadlast = '#{@game.away_last_game}'")
+					filter[2] = @game.away_last_game
+				else
+					search_string.push("roadlast <> '#{@game.away_last_game}'")
+				end
 			end
-			if filter[3]
-				search_string.push("roadnext = '#{@game.away_next_game}'")
-				search_second_string.push("roadnext = '#{@game.away_next_game}'")
-				filter[3] = @game.away_next_game
-			else
-				search_string.push("roadnext <> '#{@game.away_next_game}'")
+			if @game.away_next_game
+				if filter[3]
+					search_string.push("roadnext = '#{@game.away_next_game}'")
+					search_second_string.push("roadnext = '#{@game.away_next_game}'")
+					filter[3] = @game.away_next_game
+				else
+					search_string.push("roadnext <> '#{@game.away_next_game}'")
+				end
 			end
-			if filter[4]
-				search_string.push("homenext = '#{@game.home_next_game}'")
-				search_second_string.push("homenext = '#{@game.home_next_game}'")
-				filter[4] = @game.home_next_game
-			else
-				search_string.push("homenext <> '#{@game.home_next_game}'")
+			if @game.home_next_game
+				if filter[4]
+					search_string.push("homenext = '#{@game.home_next_game}'")
+					search_second_string.push("homenext = '#{@game.home_next_game}'")
+					filter[4] = @game.home_next_game
+				else
+					search_string.push("homenext <> '#{@game.home_next_game}'")
+				end
 			end
-			if filter[5]
-				search_string.push("homelast = '#{@game.home_last_game}'")
-				search_second_string.push("homelast = '#{@game.home_last_game}'")
-				filter[5] = @game.home_last_game
-			else
-				search_string.push("homelast <> '#{@game.home_last_game}'")
+			if @game.home_last_game
+				if filter[5]
+					search_string.push("homelast = '#{@game.home_last_game}'")
+					search_second_string.push("homelast = '#{@game.home_last_game}'")
+					filter[5] = @game.home_last_game
+				else
+					search_string.push("homelast <> '#{@game.home_last_game}'")
+				end
 			end
-			if filter[6]
-				search_string.push("homenextfly = '#{@game.home_next_fly}'")
-				search_second_string.push("homenextfly = '#{@game.home_next_fly}'")
-				filter[6] = @game.home_next_fly[0]
-			else
-				search_string.push("homenextfly <> '#{@game.home_next_fly}'")
+			if @game.home_next_fly
+				if filter[6]
+					search_string.push("homenextfly = '#{@game.home_next_fly}'")
+					search_second_string.push("homenextfly = '#{@game.home_next_fly}'")
+					filter[6] = @game.home_next_fly[0]
+				else
+					search_string.push("homenextfly <> '#{@game.home_next_fly}'")
+				end
 			end
-			if filter[7]
-				search_string.push("homelastfly = '#{@game.home_last_fly}'")
-				search_second_string.push("homelastfly = '#{@game.home_last_fly}'")
-				filter[7] = @game.home_last_fly[0]
-			else
-				search_string.push("homelastfly <> '#{@game.home_last_fly}'")
+			if @game.home_last_fly
+				if filter[7]
+					search_string.push("homelastfly = '#{@game.home_last_fly}'")
+					search_second_string.push("homelastfly = '#{@game.home_last_fly}'")
+					filter[7] = @game.home_last_fly[0]
+				else
+					search_string.push("homelastfly <> '#{@game.home_last_fly}'")
+				end
 			end
 			search_string = search_string.join(" AND ")
 			search_second_string = search_second_string.join(" AND ")
