f23c2e28fdb5f71b81839b11ad20d9b470e8fb0e
Apollo <super.hero2017@outlook.com>
2019-02-10 09:24:16 -0500

    Fix

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -19,6 +19,20 @@ class IndexController < ApplicationController
 
     @games = Nba.where("game_date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
                  .order("game_date", "home_number")
+  end
+
+  def birthday
+    unless params[:id]
+      params[:id] = Time.now.strftime("%Y-%m-%d") + " - " + Time.now.strftime("%Y-%m-%d")
+    end
+    @game_index = params[:id]
+    @game_start_index = @game_index[0..9]
+    @game_end_index = @game_index[13..23]
+
+    @head = Date.strptime(@game_start_index, '%Y-%m-%d').strftime("%B %e")
+
+    @games = Nba.where("game_date between ? and ?", Date.strptime(@game_start_index).beginning_of_day, Date.strptime(@game_end_index).end_of_day)
+                 .order("game_date", "home_number")
 
     @today = Date.strptime(@game_start_index)
     @records = []
@@ -33,24 +47,24 @@ class IndexController < ApplicationController
         @cbb_players.each_with_index do |cbb_player, index|
           if index == 0
             @records.push({
-                current: @start_date,
-                date: @start_date,
-                player: cbb_player
-            })
+                              current: @start_date,
+                              date: @start_date,
+                              player: cbb_player
+                          })
           else
             @records.push({
-                current: @start_date,
-                date: '-',
-                player: cbb_player
-            })
+                              current: @start_date,
+                              date: '-',
+                              player: cbb_player
+                          })
           end
         end
       else
         @records.push({
-            current: @start_date,
-            date: @start_date,
-            player: 'NONE'
-        })
+                          current: @start_date,
+                          date: @start_date,
+                          player: 'NONE'
+                      })
       end
 
       @nba_players = NbaPlayer.where("birthdate like '" + @start_date.strftime("%b %-d") + ",%'")
@@ -58,24 +72,24 @@ class IndexController < ApplicationController
         @nba_players.each_with_index do |nba_player, index|
           if index == 0
             @nba_records.push({
-                current: @start_date,
-                date: @start_date,
-                player: nba_player
-            })
+                                  current: @start_date,
+                                  date: @start_date,
+                                  player: nba_player
+                              })
           else
             @nba_records.push({
-                current: @start_date,
-                date: '-',
-                player: nba_player
-            })
+                                  current: @start_date,
+                                  date: '-',
+                                  player: nba_player
+                              })
           end
         end
       else
         @nba_records.push({
-            current: @start_date,
-            date: @start_date,
-            player: 'NONE'
-        })
+                              current: @start_date,
+                              date: @start_date,
+                              player: 'NONE'
+                          })
       end
       @start_date = @start_date + 1.days
     end
