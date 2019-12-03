12c7432c1e758d84240a559bae771b46c7b4bf46
Apollo <super.hero2017@outlook.com>
2019-12-03 09:27:55 +0100

    Test

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -17,7 +17,21 @@ class FilterController < ApplicationController
     @game_index = params[:date]
     @game_start_index = @game_index[0..12]
     @game_end_index = @game_index[15..27]
+
     @games = Nba.where("game_date between ? and ? AND id >= 26796", Date.strptime(@game_start_index, '%b %d, %Y').beginning_of_day, Date.strptime(@game_end_index, '%b %d, %Y').end_of_day)
-                  .or(Nba.where("game_date between ? and ? AND id <= 26573 AND id >= 25261", Date.strptime(@game_start_index, '%b %d, %Y').beginning_of_day, Date.strptime(@game_end_index, '%b %d, %Y').end_of_day)).order('id DESC')
+               .or(Nba.where("game_date between ? and ? AND id <= 26573 AND id >= 25261", Date.strptime(@game_start_index, '%b %d, %Y').beginning_of_day, Date.strptime(@game_end_index, '%b %d, %Y').end_of_day)).order('id DESC')
+
+    @teams = Team.all.order('team')
+    @home_team = nil
+    @away_team = nil
+    if params[:home_team_id].present?
+      @home_team = Team.find_by(id: params[:home_team_id])
+      @games = @games.where("home_team = ?", @home_team.team)
+    end
+
+    if params[:away_team_id].present?
+      @away_team = Team.find_by(id: params[:away_team_id])
+      @games = @games.where("away_team = ?", @away_team.team)
+    end
   end
 end
