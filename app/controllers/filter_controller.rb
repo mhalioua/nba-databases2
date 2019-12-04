0324ff89a119f35b06afae81aa0f8f925de3af3e
Apollo <super.hero2017@outlook.com>
2019-12-04 09:13:07 +0100

    Fix

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -26,7 +26,7 @@ class FilterController < ApplicationController
     @games = Nba.where("id >= 26796").or(Nba.where("id <= 26573 AND id >= 25261")).order('id DESC')
 
     if @datePicker
-      @games = @games.where("game_date <= ?", Date.current)
+      @games = @games.where("game_date <= ?", Date.today.end_of_day)
     else
       @games = @games.where("game_date between ? and ?", Date.strptime(@game_start_index, '%b %d, %Y').beginning_of_day, Date.strptime(@game_end_index, '%b %d, %Y').end_of_day)
     end
