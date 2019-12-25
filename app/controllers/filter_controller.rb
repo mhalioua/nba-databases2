85b2591f6d60313995a7530083b4c8dbb17049bd
Apollo <super.hero2017@outlook.com>
2019-12-25 09:10:32 +0100

    Test

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -33,6 +33,10 @@ class FilterController < ApplicationController
     @teams = Team.all.order('team')
     @home_team_id = 0
     @away_team_id = 0
+    @last_city_home = 0
+    @last_city_away = 0
+    @next_city_home = 0
+    @next_city_away = 0
     if params[:home_team_id].present?
       @home_team_id = params[:home_team_id].to_i
       @home_team = Team.find_by(id: params[:home_team_id])
@@ -52,6 +56,50 @@ class FilterController < ApplicationController
         away_team_name = 'LAC' if away_team_name == 'LA Clippers'
         away_team_name = 'LAL' if away_team_name == 'LA Lakers'
         @games = @games.where("away_team = ?", away_team_name)
+      end
+    end
+
+    if params[:last_city_home].present?
+      @last_city_home = params[:last_city_home].to_i
+      @last_city_home_team = Team.find_by(id: params[:last_city_home])
+      if @last_city_home_team != nil
+        last_city_home_team_name = @last_city_home_team.team
+        last_city_home_team_name = 'LAC' if last_city_home_team_name == 'LA Clippers'
+        last_city_home_team_name = 'LAL' if last_city_home_team_name == 'LA Lakers'
+        @games = @games.where("home_team_city = ?", last_city_home_team_name).or(@games.where("home_team_city = 'home' AND home_team = ?", last_city_home_team_name))
+      end
+    end
+
+    if params[:last_city_away].present?
+      @last_city_away = params[:last_city_away].to_i
+      @last_city_away_team = Team.find_by(id: params[:last_city_away])
+      if @last_city_away_team != nil
+        last_city_away_team_name = @last_city_away_team.team
+        last_city_away_team_name = 'LAC' if last_city_away_team_name == 'LA Clippers'
+        last_city_away_team_name = 'LAL' if last_city_away_team_name == 'LA Lakers'
+        @games = @games.where("away_team_city = ?", last_city_away_team_name).or(@games.where("away_team_city = 'home' AND away_team = ?", last_city_away_team_name))
+      end
+    end
+
+    if params[:next_city_home].present?
+      @next_city_home = params[:next_city_home].to_i
+      @next_city_home_team = Team.find_by(id: params[:next_city_home])
+      if @next_city_home_team != nil
+        next_city_home_team_name = @next_city_home_team.team
+        next_city_home_team_name = 'LAC' if next_city_home_team_name == 'LA Clippers'
+        next_city_home_team_name = 'LAL' if next_city_home_team_name == 'LA Lakers'
+        @games = @games.where("home_team_city = ?", next_city_home_team_name).or(@games.where("home_team_city = 'home' AND home_team = ?", next_city_home_team_name))
+      end
+    end
+
+    if params[:next_city_away].present?
+      @next_city_away = params[:next_city_away].to_i
+      @next_city_away_team = Team.find_by(id: params[:next_city_away])
+      if @next_city_away_team != nil
+        next_city_away_team_name = @next_city_away_team.team
+        next_city_away_team_name = 'LAC' if next_city_away_team_name == 'LA Clippers'
+        next_city_away_team_name = 'LAL' if next_city_away_team_name == 'LA Lakers'
+        @games = @games.where("away_team_city = ?", next_city_away_team_name).or(@games.where("away_team_city = 'home' AND away_team = ?", next_city_away_team_name))
       end
     end
   end
