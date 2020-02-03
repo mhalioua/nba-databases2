7f56601bc9c34f8efdd86bcb95416fe4d5b2e0e3
Hammad Maqbool <hammad417@gmail.com>
2020-02-03 19:41:25 +0500

    imgkit width and height changed

--- lib/heroku_aws.rb
+++ lib/heroku_aws.rb
@@ -2,7 +2,7 @@
 		def heroku_upload_images(games)
 			games.each do |game|
 				if game.filters.any?
-					kit = IMGKit.new("https://nba-databases.herokuapp.com/index/rest_view/#{game.game_id}", :width => 1900, :height => 3000)
+					kit = IMGKit.new("https://nba-databases.herokuapp.com/index/rest_view/#{game.game_id}",:quality => 50, :width => 1800, :height => 2800)
 					file = kit.to_file("#{Rails.root}/tmp/rest_view.png")
 					obj = S3.object("imgaes_new/#{game.game_id}.png")
 					obj.upload_file(file, acl:'public-read')
