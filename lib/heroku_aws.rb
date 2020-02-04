bf08518735a19c65837726086f1785410383849f
Hammad Maqbool <hammad417@gmail.com>
2020-02-04 13:14:12 +0500

    image upload task code updated

--- lib/heroku_aws.rb
+++ lib/heroku_aws.rb
@@ -1,8 +1,8 @@
 	class HerokuAws
-		def heroku_upload_images(games)
+		def self.heroku_upload_images(games)
 			games.each do |game|
 				if game.filters.any?
-					kit = IMGKit.new("https://nba-databases.herokuapp.com/index/rest_view/#{game.game_id}",:quality => 50, :width => 1800, :height => 2800)
+					kit = IMGKit.new("https://nba-databases.herokuapp.com/index/rest_view/#{game.game_id}")
 					file = kit.to_file("#{Rails.root}/tmp/rest_view.png")
 					obj = S3.object("imgaes_new/#{game.game_id}.png")
 					obj.upload_file(file, acl:'public-read')
