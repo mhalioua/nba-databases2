f2df1780d6fffca865aa330d906590b3494f89d8
Hammad Maqbool <hammad417@gmail.com>
2020-02-24 18:38:16 +0500

    generate xls file code updated

--- lib/nba_data_sheet.rb
+++ lib/nba_data_sheet.rb
@@ -2,8 +2,10 @@ class NbaDataSheet
 	def self.generate_data_sheet(game_start_date,game_end_date)
 		require 'rubygems'
 		require 'spreadsheet'
-		book = Spreadsheet::Workbook.new 
-   		sheet1 = book.create_worksheet :name => "Sheet1"
+    data_file_path = [Rails.root, "csv", "nba_bday_12.27_feb_26_2.xls"].join("/")
+		#book = Spreadsheet::Workbook.new 
+   	book = Spreadsheet.open(data_file_path)
+    sheet1 = book.create_worksheet :name => "MAIN (6)"
    		header = [nil, "Year", "Date", "Time", "Week", "tv2", "tv", "count", "is last game home", "last", "next", "is next game home",
    			   "Away Team", "away_win_rank", true, "last city", "next city", "away_ppg_rank", "away_oppppg_rank", "FROM", "TO", "1Q", "2Q",
    			   nil, "3Q", nil, "4Q", "OT", nil, "next", "FLY", "last", "FLY", "Home Team", "home_win_rank", nil, "last city", "next city",
@@ -38,8 +40,8 @@ class NbaDataSheet
 
    		sheet1.insert_row(0, header)
    		
-      #game = Nba.find([1,2])
-		  games = Nba.where("game_date between ? and ?", Date.strptime(game_start_date).beginning_of_day, Date.strptime(game_end_date).end_of_day)   		
+      games = Nba.find([1,2])
+		  #games = Nba.where("game_date between ? and ?", Date.strptime(game_start_date).beginning_of_day, Date.strptime(game_end_date).end_of_day)   		
    		games.each_with_index  { |game, i| 
    		date = DateTime.parse(game.game_date)
       day_month = date.strftime('%d-%b')[0]=="0" ? date.strftime('%d-%b')[1..5] : date.strftime('%d-%b')
@@ -100,11 +102,74 @@ class NbaDataSheet
 
      			]
    		}        
-
-   		export_file_path = [Rails.root, "public", "exports", "nba_databases_data.xls"].join("/")
-   		book.write export_file_path
-      obj = S3.object("nba_data/nba_data_sheet.xls")
-      obj.upload_file(export_file_path, acl:'public-read')
-      return obj.public_url
+      File.delete(data_file_path)
+   		#export_file_path = [Rails.root, "public", "exports", "nba_databases_data.xls"].join("/")
+   		book.write data_file_path
+      #obj = S3.object("nba_data/nba_data_sheet.xls")
+      #obj.upload_file(export_file_path, acl:'public-read')
+      #return obj.public_url
 	end
+
+  def self.update_sheet_last_week_data
+    nba_data_file_path = [Rails.root, "public", "exports", "nba_databases_data.xls"].join("/")
+    book = Spreadsheet.open(nba_data_file_path)
+    sheet1 = book.worksheet(0)
+    game_start_date = (Date.today - 7.days).to_s
+    game_end_index = (Date.today).to_s
+    games = Nba.where("game_date between ? and ?", Date.strptime(game_start_date).beginning_of_day, Date.strptime(game_end_date).end_of_day)      
+    sheet_row = sheet1.last_row_index + 1
+    games.each do |game|
+      row_to_insert = ['',date.strftime('%Y'),date.strftime('%b %d'),date.strftime('%I:%M %p'),date.strftime('%a'),game.tv_station.nil? ? '' : game.tv_station.split(",")[0],
+            game.tv_station.nil? ? '' : game.tv_station.split(",")[1],game.game_count,game.away_last_fly,game.away_last_game,game.away_next_game,
+            game.away_next_fly,game.away_team,game.away_win_rank,'',game.away_team_city,game.away_team_next_city,game.away_ppg_rank,
+            game.away_oppppg_rank,game.away_timezone,game.home_timezone,game.away_first_quarter,game.away_second_quarter,away_first_second_quarter,
+            game.away_third_quarter,away_first_sec_third_quarter,game.away_forth_quarter,
+            game.away_ot_quarter,'',game.home_next_game,game.home_next_fly,game.home_last_game,game.home_last_fly,game.home_team,
+            game.home_win_rank,'',game.home_team_city,game.home_team_next_city,game.home_ppg_rank,game.home_oppppg_rank,game.home_timezone,
+            game.home_first_quarter,game.home_second_quarter,home_first_second_quarter,game.home_third_quarter,
+            home_first_sec_third_quarter,game.home_forth_quarter,'',game.home_ot_quarter,
+            '','','','','',game.away_score,game.home_score,away_home_score,game.first_point,game.second_point,
+            '','','','',game.total_point,first_ou,second_ou,total_ou,
+            game.first_closer_total,game.second_closer_total,game.full_closer_total,game.first_half_bigger,
+            game.second_half_bigger,game.fullgame_bigger,game.first_closer_side,game.second_closer_side,'',game.full_closer_side,
+            game.home_last_ot,game.away_last_ot,'','','','','','','','','','','','','','','','','','','','','','','','','','','','',
+            '','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',
+            game.pace,game.away_ortg,game.home_ortg,game.away_last_home,game.away_next_home,away_last_next_home,
+            '',game.home_last_away,game.home_next_away,home_last_next_away,'','','','','',game.fg_road_2000,game.fg_home_2000,
+            game.fg_diff_2000,game.fg_count_2000,'',game.fg_road_1990,game.fg_home_1990,game.fg_diff_1990,game.fg_count_1990,game.first_half_road_2000,
+            game.first_half_home_2000,game.first_half_diff_2000,game.first_half_count_2000,'',game.first_half_road_1990,game.first_half_home_1990,
+            game.first_half_diff_1990,game.first_half_count_1990,game.second_half_road_2000,game.second_half_home_2000,game.second_half_diff_2000,
+            game.second_half_count_2000,'',game.second_half_road_1990,game.second_half_home_1990,game.second_half_diff_1990,game.second_half_count_1990,
+            '',game.fg_total_pt_2000,game.fg_total_line_2000,game.fg_total_diff_2000,game.fg_total_count_2000,game.first_half_total_pt_2000,
+            game.first_half_total_line_2000,game.first_half_total_diff_2000,game.first_half_total_count_2000,game.second_half_total_pt_2000,
+            game.second_half_total_line_2000,game.second_half_total_diff_2000,game.second_half_total_count_2000,game.fg_total_pt_1990,game.fg_total_line_1990,
+            game.first_half_total_pt_1990,game.first_half_total_line_1990,game.second_half_total_pt_1990,game.second_half_total_line_1990,
+            '','','','','','','','','',game.away_player1_name,game.away_player1_birthday.nil? ? '' : game.away_player1_birthday[0..11],
+            game.away_player2_name,game.away_player2_birthday.nil? ? '' : game.away_player2_birthday[0..11],
+            game.away_player3_name,game.away_player3_birthday.nil? ? '' : game.away_player3_birthday[0..11],
+            game.away_player4_name,game.away_player4_birthday.nil? ? '' : game.away_player4_birthday[0..11],
+            game.away_player5_name,game.away_player5_birthday.nil? ? '' : game.away_player5_birthday[0..11],
+            game.away_player6_name,game.away_player6_birthday.nil? ? '' : game.away_player6_birthday[0..11],
+            game.away_player7_name,game.away_player7_birthday.nil? ? '' : game.away_player7_birthday[0..11],
+            game.away_player8_name,game.away_player8_birthday.nil? ? '' : game.away_player8_birthday[0..11],
+            game.home_player1_name,game.home_player1_birthday.nil? ? '' : game.home_player1_birthday[0..11],
+            game.home_player2_name,game.home_player2_birthday.nil? ? '' : game.home_player2_birthday[0..11],
+            game.home_player3_name,game.home_player3_birthday.nil? ? '' : game.home_player3_birthday[0..11],
+            game.home_player4_name,game.home_player4_birthday.nil? ? '' : game.home_player4_birthday[0..11],
+            game.home_player5_name,game.home_player5_birthday.nil? ? '' : game.home_player5_birthday[0..11],
+            game.home_player6_name,game.home_player6_birthday.nil? ? '' : game.home_player6_birthday[0..11],
+            game.home_player7_name,game.home_player7_birthday.nil? ? '' : game.home_player7_birthday[0..11],
+            game.home_player8_name,game.home_player8_birthday.nil? ? '' : game.home_player8_birthday[0..11]
+
+          ]
+          sheet1.insert_row(sheet_row, row_to_insert)
+          sheet_row = sheet_row + 1
+    end
+    File.delete(nba_data_file_path)
+    # Write out the Workbook again
+    book.write(nba_data_file_path)
+    obj = S3.object("nba_data/nba_data_sheet.xls")
+    obj.upload_file(nba_data_file_path, acl:'public-read')
+    puts obj.public_url
+  end
 end
