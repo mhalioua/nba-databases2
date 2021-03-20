f2d224bba6369b690769001178a2e45a994a896b
Apollo <super.hero2007@caramail.com>
2021-03-20 11:21:22 +0100

    fix: fixed script

--- db/migrate/20210320105016_add_coulmn_to_wnba.rb
+++ db/migrate/20210320105016_add_coulmn_to_wnba.rb
@@ -1,4 +1,4 @@
-class AddCoulmnToNba < ActiveRecord::Migration[5.1]
+class AddCoulmnToWnba < ActiveRecord::Migration[5.1]
   def change
   	add_column :wnbas, :home_team_timezone, :string
     add_column :wnbas, :away_team_timezone, :string
